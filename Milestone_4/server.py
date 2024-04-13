c34a7be3b75b1c4ab38721753d72232405666c99
jw7914 <124104246+jw7914@users.noreply.github.com>
2024-04-13 02:24:14 -0400

    Able to connect to db back end

--- Milestone_4/server.py
+++ Milestone_4/server.py
@@ -1,6 +1,8 @@
-#Import Flask Library
+#Import Libraries
 from flask import Flask, render_template, request, session, url_for, redirect
 import pymysql.cursors
+from flask_mysqldb import MySQL 
+import pandas as PD
 
 #Initialize the app from Flask
 app = Flask(__name__)
@@ -13,10 +15,22 @@ conn = pymysql.connect(host='localhost',
                        charset='utf8mb4',
                        cursorclass=pymysql.cursors.DictCursor)
 
+def runstatement(query):
+    cursor = conn.cursor()
+    cursor.execute(query)
+    results = cursor.fetchall()
+    conn.commit()
+    column_names = [desc[0] for desc in cursor.description]
+    # Create a DataFrame
+    df = PD.DataFrame(results, columns=column_names)
+    # Close the cursor
+    cursor.close()
+    return df
 
-@app.route('/')
+# NEED TO DO conn.close() at the end of each app.route
+@app.route('/', methods=['POST', 'GET'])
 def home():
-	return render_template('base.html')
+	return render_template('home_test.html')
 
 @app.route('/login')
 def login():
@@ -25,6 +39,17 @@ def login():
 @app.route('/help')
 def help():
 	return render_template('help.html')
+
+@app.route('/test', methods=['POST', 'GET'])
+def test():
+	first_name = request.get_data
+	df = runstatement("SELECT * FROM CRIMINAL;")
+	criminal_ids = []
+	for i, j in df.iterrows():
+		criminal_ids.append(j['criminal_id'])
+	return render_template('test.html', criminal_ids=criminal_ids)
+
+
 
 
 if __name__ == "__main__":
