12ebeea5b4624e141633acdd43c1fec7695ef9c3
jw7914 <jw7914@nyu.edu>
2024-04-17 14:39:58 -0400

    outlining privilege ideas

--- Milestone_4/privileges.sql
+++ Milestone_4/privileges.sql
@@ -1,7 +1,11 @@
 -- Create public view
--- No permissions to change table aka select user from mysql.user;
+-- No permissions to change table aka  INSERT, UPDATE, DELETE
+-- No permission to view personal information
+-- Only update permission to update fines?
 
 -- Create officer view
-
+-- No permission to view users
+-- No permission to change tables only be able to view stuff
 
 -- Admin view = root user nothing to be done
+-- Overall manager to manage officers and assign them to certain things
